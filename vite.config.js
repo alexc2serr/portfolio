81b94f3484a769a552a8750184bd6c28b976170a
Alejandro Serrano Calvo <aserrano@fcirce.es>
2026-02-03 13:06:49 +0100

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // Reemplaza 'portfolio' con el nombre exacto de tu repositorio en GitHub
+  base: '/alexc2serr/',  
 })
